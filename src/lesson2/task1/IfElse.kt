0452f3aa1085718cc14861329bb71f49941f6289
Yerdaulyet <112725271+Yerdaulyet@users.noreply.github.com>
2022-12-27 08:30:06 +0300

    lesson 2

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -162,15 +162,15 @@ fun rookOrBishopThreatens(
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int  {
-    if((a > b + c) || (b > a + c) || (c > b +a))
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    if((a > b + c) || (b > a + c) || (c > b + a))
         return -1
     val max: Double
     val sum: Double
     if (a > b && a > c) {
         max = a * a
         sum = b * b + c * c
-    } else if (b > c){
+    } else if (b > c) {
         max = b * b
         sum = a * a + c * c
     } else {
